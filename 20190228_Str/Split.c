952993de7a25fd232d815c506622da5fe9f50474
wangze <w18829898295@163.com>
2019-03-11 10:30:36 +0800

    为了使strtok使用方便,Split

---
+++ 20190228_Str/Split.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+//封装函数Split,使得strtok方便使用
+//返回值是切分出来的token个数
+int Split(char* input, const char* split_char, char* output[]){
+  int output_index = 0;
+  char* p = strtok(input, split_char);
+  while(p != NULL){
+    output[output_index] = p;       //指针传递
+    ++output_index;
+    p = strtok(NULL, split_char);
+  }
+  return output_index;
+}
+
+int main(){
+  char str[] = "ls -l /";
+  char* output[10] = {0};           //定义了一个指针数组
+  int n = Split(str, " ", output);
+  int i = 0;
+  for(; i < n; ++i){
+    printf("%s\n", output[i]);
+  }
+  return 0;
+}
