57aced33880a28a9e04d2ca5ba647a22ee326cd4
wangze <w18829898295@163.com>
2019-03-26 21:32:52 +0800

    父子进程有关wait

---
+++ 20190324/wait.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main(){
+  int pid = fork();
+  if(pid < 0){
+    perror("fork errno");
+    exit(-1);
+  }
+  else if(pid == 0){
+    sleep(5);
+    exit(0);
+  }
+  //pid_t wait(int *status);
+  //等待任意一个子进程退出
+  //status:用于获取退出返回值
+  //返回值:返回退出子进程的pid; 出错:返回-1
+  int status;
+//wait(&status);
+  //pid_t waitpid(pid_t pid, int *ststus, int options);
+  //pid: 指定的进程id
+  //    -1:等待任意进程
+  //    >0:等待指定子进程
+  //status:用于获取返回值
+  //options:选项
+  //    WNOHANG:将waitpid设置为非阻塞
+  //返回值: <0:出错   ==0:没有子进程退出    >0:退出子进程的pid
+  //if(waitpid(pid,&status,0) < 0){
+  //  perror("waitpid error");
+  //}
+  while(waitpid(pid,&status,WNOHANG) == 0){
+    printf("no exit ~~ smoking ~~\n");
+    sleep(1);
+  }
+  while(1){
+    printf("da ma jiang\n");
+    sleep(1);
+  }
+  return 0;
+}
