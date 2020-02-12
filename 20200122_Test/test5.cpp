c49c36f0359ba23c1824a6fa4de26dd7bc28e92a
WZ-Dale <w18829898295@163.com>
2020-02-12 21:34:17 +0800

    字符处理函数

--- 20200122_Test/test5.cpp
+++ 20200122_Test/test5.cpp
@@ -4,10 +4,37 @@
 
 using namespace std;
 
+int Tolower(int c){
+    if(c >= 'A' && c <= 'Z'){
+        c += 'a' - 'A';
+    }
+    return c;
+}
+int Toupper(int c){
+    if(c >= 'a' && c <= 'z'){
+        c -= 'a' - 'A';
+    }
+    return c;
+}
+
 int main(){
     FILE* fp = fopen("test.txt", "r");
     printf("%d\n", errno);
     printf("%s\n", strerror(errno));
     printf("%s\n", strerror(2));
+
+    char str[] = "aBcdEFG";
+    for(auto& e : str){
+        //char c = tolower((int)e);
+        char c = Tolower((int)e);
+        cout << c;
+    }
+    cout << endl;
+    for(auto& e : str){
+        //char c = toupper((int)e);
+        char c = Toupper((int)e);
+        cout << c;
+    }
+    cout << endl;
     return 0;
 }
