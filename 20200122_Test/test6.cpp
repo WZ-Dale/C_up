e76c111e1f310fcda3a824652f2f0c07edceb294
WZ-Dale <w18829898295@163.com>
2020-02-14 22:18:35 +0800

    数组中存放的并不是变量，而是变量中数值的拷贝

--- 20200122_Test/test6.cpp
+++ 20200122_Test/test6.cpp
@@ -10,5 +10,14 @@ int main(){
         WHAT
     };
     cout << MALE << FEMALE << UNKNOWN << WHAT << endl;
+
+    int a = 2, b = 3;
+    int arr[] = {1, a, 3};
+    cout << arr[1] << endl;
+    printf("%p\n", &a);
+    printf("%p\n", &b);
+    printf("%p\n", &arr[0]);
+    printf("%p\n", &arr[1]);
+    printf("%p\n", &arr[2]);
     return 0;
 }
